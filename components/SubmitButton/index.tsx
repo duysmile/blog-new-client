97518e45722cbaaee77ec23e89eb8395a3464134
duy.nguyen <duy.nguyen@vnlp.ai>
2021-07-08 23:33:23 +0700

    add login page

--- components/SubmitButton/index.tsx
+++ components/SubmitButton/index.tsx
@@ -2,13 +2,15 @@ import { Button } from "@moai/core";
 
 type Props = {
     children: React.ReactNode;
+    busy: boolean;
 }
 
-export default function SubmitButton({ children }: Props) {
+export default function SubmitButton({ children, busy }: Props) {
     return (
         <Button
             type="submit"
             fill={true}
+            busy={busy}
             highlight
         >
             {children}
---
+++ pages/login/errors.tsx
@@ -0,0 +1,6 @@
+const LOGIN_ERRORS = {
+    emailRequired: "Email is required",
+    passwordRequired: "Password is required",
+};
+
+export default LOGIN_ERRORS;
